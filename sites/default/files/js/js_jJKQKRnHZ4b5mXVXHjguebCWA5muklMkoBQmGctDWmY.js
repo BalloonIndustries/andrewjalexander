035f74fe7441fb2123ca52904c1e6ebe8b13ed3e
Andrew Alexander <andy@balloonindustries.com>
2015-03-20 11:18:13 +0000

    update to 7.35 and removing viruses

---
+++ sites/default/files/js/js_jJKQKRnHZ4b5mXVXHjguebCWA5muklMkoBQmGctDWmY.js
@@ -0,0 +1,49 @@
+jQuery(document).ready(function(){
+jQuery("#fbicon").hover(
+function () {
+    jQuery(this).attr("src","http://andrewjalexander.com/sites/all/themes/andy/images/nohoverfb.png");
+},
+function () {
+    jQuery(this).attr("src","http://andrewjalexander.com/sites/all/themes/andy/images/fb.png");
+});
+
+jQuery("#linkedinicon").hover(
+function () {
+    jQuery(this).attr("src","http://andrewjalexander.com/sites/all/themes/andy/images/nohoverlinkedin.png");
+},
+function () {
+    jQuery(this).attr("src","http://andrewjalexander.com/sites/all/themes/andy/images/linkedin.png");
+});
+
+jQuery("#googleplusicon").hover(
+function () {
+    jQuery(this).attr("src","http://andrewjalexander.com/sites/all/themes/andy/images/nohovergoogle+.png");
+},
+function () {
+    jQuery(this).attr("src","http://andrewjalexander.com/sites/all/themes/andy/images/google+.png");
+});
+
+jQuery("#youtubeicon").hover(
+function () {
+    jQuery(this).attr("src","http://andrewjalexander.com/sites/all/themes/andy/images/nohoveryoutube.png");
+},
+function () {
+    jQuery(this).attr("src","http://andrewjalexander.com/sites/all/themes/andy/images/youtube.png");
+});
+
+jQuery("#twittericon").hover(
+function () {
+    jQuery(this).attr("src","http://andrewjalexander.com/sites/all/themes/andy/images/nohovertwitter.png");
+},
+function () {
+    jQuery(this).attr("src","http://andrewjalexander.com/sites/all/themes/andy/images/twitter.png");
+});
+
+jQuery("#emailicon").hover(
+function () {
+    jQuery(this).attr("src","http://andrewjalexander.com/sites/all/themes/andy/images/nohoveremail.png");
+},
+function () {
+    jQuery(this).attr("src","http://andrewjalexander.com/sites/all/themes/andy/images/email.png");
+});});
+;
---
+++ sites/default/files/styles/files.php
@@ -0,0 +1,5 @@
+<?php
+
+echo "WHAT UP!!";
+
+?>
