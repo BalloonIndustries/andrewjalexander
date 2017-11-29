886b674d60dc0f99e5b0f58c4356b8af62eaaa10
Andy Alexander <andy@balloonindustries.com>
2017-11-29 16:51:24 -0500

    Works JS

--- sites/all/themes/andy/js/andy.js
+++ sites/all/themes/andy/js/andy.js
@@ -5,8 +5,6 @@
                 $('.page-projects .views-field-field-project-image img').hover(handlerIn, handlerOut);
             } else {
                 $('.page-projects .views-field-field-project-image img').css("filter", "brightness(100%)");
-                $(this).closest('.views-row').find(".page-projects .view-projects .views-field-title a").css('color', '#B51F24');
-                $(this).closest('.views-row').find(".page-projects .view-projects .views-field-title a").css('text-shadow', '-1px -1px 0 #000, 1px -1px 0 #000, -1px 1px 0 #000, 1px 1px 0 #000');
                 $(this).closest('.views-row').find(".views-field-title").show();
             }
             function handlerIn() {
