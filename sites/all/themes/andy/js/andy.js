b2e02930c57d9584f195ce011c4cc6627c45d753
Andy Alexander <andy@balloonindustries.com>
2017-11-29 16:41:41 -0500

    For mobile

--- sites/all/themes/andy/js/andy.js
+++ sites/all/themes/andy/js/andy.js
@@ -1,24 +1,22 @@
-(function($){
-Drupal.behaviors.andy = {
-  attach: function (context, settings) {
-      if(typeof window.orientation === "undefined") {
-$('.page-projects .views-field-field-project-image img' ).hover( handlerIn, handlerOut );
-      }
-function handlerIn() {
-        $( this ).css( "filter", "brightness(100%)" );
-        /*
-        var parents = $(this).parentsUntil('.views-column');
-        console.log(parents);
-        var last = parents.last();*/
-    $(this).closest('.views-row').find(".views-field-title").hide();
-        /*console.log(last);
-        $(last + " .views-field-title").hide();
-        console.log(last + " .views-field-title");*/
-}
-function handlerOut() {
-    $('.page-projects .views-field-field-project-image img').css("filter", "brightness(50%)");
-   $(this).closest('.views-row').find(".views-field-title").show();
-}
-}
-};
+(function ($) {
+    Drupal.behaviors.andy = {
+        attach: function (context, settings) {
+            if (typeof window.orientation === "undefined") {
+                $('.page-projects .views-field-field-project-image img').hover(handlerIn, handlerOut);
+            } else {
+                $('.page-projects .views-field-field-project-image img').css("filter", "brightness(100%)");
+                $(this).closest('.views-row').find(".page-projects .view-projects .views-field-title a").addCSS('color', '#B51F24');
+                $(this).closest('.views-row').find(".page-projects .view-projects .views-field-title a").addCSS('text-shadow', '-1px -1px 0 #000, 1px -1px 0 #000, -1px 1px 0 #000, 1px 1px 0 #000');
+                $(this).closest('.views-row').find(".views-field-title").show();
+            }
+            function handlerIn() {
+                $(this).css("filter", "brightness(100%)");
+                $(this).closest('.views-row').find(".views-field-title").hide();
+            }
+            function handlerOut() {
+                $('.page-projects .views-field-field-project-image img').css("filter", "brightness(50%)");
+                $(this).closest('.views-row').find(".views-field-title").show();
+            }
+        }
+    };
 })(jQuery);
