5d7729355faa54c7152cfe7e1611d96e381d1658
Andy Alexander <andy@balloonindustries.com>
2017-11-28 22:48:04 -0500

    No trouble

--- sites/all/themes/andy/js/andy.js
+++ sites/all/themes/andy/js/andy.js
@@ -1,10 +1,10 @@
 (function($){
 Drupal.behaviors.andy = {
   attach: function (context, settings) {
+      if(typeof window.orientation === "undefined") {
 $('.page-projects .views-field-field-project-image img' ).hover( handlerIn, handlerOut );
-
+      }
 function handlerIn() {
-
     $('.page-projects .views-field-field-project-image img').css("filter", "brightness(50%)");
         $( this ).css( "filter", "unset" );
 }
