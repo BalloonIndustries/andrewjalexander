1fc8dc3f3b0ee01a9d9dd4fb4c92f05da46898cc
Andy Alexander <andy@balloonindustries.com>
2017-11-29 19:40:44 -0500

    First day

--- sites/all/themes/andy/js/typed-welcome.js
+++ sites/all/themes/andy/js/typed-welcome.js
@@ -2,7 +2,7 @@
     Drupal.behaviors.typed = {
         attach: function (context, settings) {
             var typed = new Typed('#page-title', {
-                strings: ['Welcome', 'My name is Andy', 'I am a Developer', 'Welcome', ],
+                strings: ['Welcome', 'My name is Andy', 'I am a Developer', 'Welcome'],
                 typeSpeed: 75,
                 backSpeed: 75
             });
