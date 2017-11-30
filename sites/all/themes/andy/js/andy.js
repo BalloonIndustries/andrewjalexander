6ebaf7253d52ffcb81b6cdb9dae3e71d68a5209e
Andy Alexander <andy@balloonindustries.com>
2017-11-30 12:44:26 -0500

    Adding functionality

--- sites/all/themes/andy/js/andy.js
+++ sites/all/themes/andy/js/andy.js
@@ -4,8 +4,17 @@
             if (typeof window.orientation === "undefined") {
                 $('.page-projects .views-field-field-project-image img').hover(handlerIn, handlerOut);
             } else {
-                $('.page-projects .views-field-field-project-image img').css("filter", "brightness(100%)");
-                $('.page-projects .views-field-title .field-content').addClass('project-banner');
+                //$('.page-projects .views-field-field-project-image img').css("filter", "brightness(100%)");
+                //$('.page-projects .views-field-title .field-content').addClass('project-banner');
+                $(window).on('scroll', function () {
+                    $('.page-projects .views-field-field-project-image img').each(function () {
+                        if ($(window).scrollTop() >= $(this).offset().top) {
+                            console.log("hits target");
+                            console.log(this);
+                            $(this).css("filter", "brightness(100%)");
+                        }
+                    });
+                });
             }
             function handlerIn() {
                 $(this).css("filter", "brightness(100%)");
